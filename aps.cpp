a1e9f5f4699a3842b3a21e01021497f9a196f1a1
PhG22 <57491645+PhG22@users.noreply.github.com>
2019-11-24 23:13:25 -0300

    Update aps.cpp

--- aps.cpp
+++ aps.cpp
@@ -1,37 +1,37 @@
-Autor: Pedro Henrique Guimarães Gomes - UTFPR 2019/2
-RA: 2193000
-Fisica Teórica 1 - S71 - Prof. Rafael C. Barreto
+/*------------------------------------------------------*
+|							|
+|  Autor: Pedro Henrique Guimarães Gomes - UTFPR 2019/2	|
+|  RA: 2193000						|
+|  Fisica Teórica 1 - S71 - Prof. Rafael C. Barreto	|
+|							|
+*/------------------------------------------------------*
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <math.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
+#include <time.h>
 
-#define NAME "APS_SINUCA"
-#define DIRETORIO_IMAGEM "bola.png" //caminho para o "sprite" da bola
+#include <D:/Dev/SDL2/include/SDL.h>
+#include <D:/Dev/SDL2_image/include/SDL_image.h>
 
+#define NAME "APS Colisoes"
+#define TEXTURE_DIR "D:/aps/APS BARRETO/APS BARRETO/bola.png"
 
 typedef struct {
 
-    double x;
-    double y;
+	double x;
+	double y;
+	double vx;
+	double vy;
 
-    double vx;
-    double vy;
-
-    int r;
-
+	int r;
 }Bola;
-
-
 
 int raio;
 
 SDL_Window* window = NULL;
 
-SDL_Window* render = NULL;
+SDL_Renderer* render = NULL;
 
 SDL_Surface* imagem_bola = NULL;
 
@@ -39,328 +39,297 @@ SDL_Texture* textura_bola = NULL;
 
 SDL_Rect bola_atual;
 
+void criaJanela(int WINDOW_WIDTH, int WINDOW_HEIGHT) {
 
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		printf("Erro ao abrir o video\n");
 
-//cria a janela e a superficie branca
-void criaJanela(int WINDOW_WIDTH, int WINDOW_HEIGHT){
+	else {
+		window = SDL_CreateWindow(NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+		if (window == NULL)
+			printf("Erro ao criar a janela\n");
 
-        printf("erro abrindo video\n");
-    }
-    else{
+		else {
+			render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-        window = SDL_CreateWindow(NAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+			if (render == NULL)
+				printf("Erro ao criar o render\n");
 
-        if(window == NULL){
-
-            printf("erro criando a window\n");
-        }
-        else{
-
-            render = SDL_CreateWindow(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-            if(render == NULL){
-
-                printf("erro criando render\n");
-            }
-            else{
-
-                SDL_SetRenderDrawColor( render, 0xFF, 0xFF, 0xFF, 0xFF);
-            }
-        }
-    }
+			else
+				SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+		}
+	}
 }
 
-//aplica a textura da bola
-void aplicaTextura(){
+void aplicaTextura(void) {
 
-    int imgFlags = IMG_INIT_PNG;
+	int imgFlags = IMG_INIT_PNG;
 
-    if(!(IMG_Init(imgFlags) & imgFlags)){
+	if (!(IMG_Init(imgFlags) & imgFlags))
+		printf("Erro ao abrir o header da imagem\n");
 
-        printf( "erro abrindo header imagem\n");
-    }
-    else{
+	else {
+		imagem_bola = IMG_Load(TEXTURE_DIR);
 
-        imagem_bola = IMG_Load(DIRETORIO_IMAGEM);
+		if (imagem_bola == NULL)
+			printf("Erro ao abrir a imagem\n");
 
-        if(imagem_bola == NULL){
+		else {
 
-            printf("erro abrindo imagem\n");
-        }
-        else{
+			raio = imagem_bola->h / 2;
 
-            raio = imagem_bola->h /2;
+			textura_bola = SDL_CreateTextureFromSurface(render, imagem_bola);
 
-            textura_bola = SDL_CreateTextureFromSurface(render, imagem_bola);
+			if (textura_bola == NULL)
+				printf("Erro ao carregar a textura");
 
-            if(textura_bola == NULL){
+		}
+	}
 
-                printf("erro carregando textura\n");
-            }
-        }
-    }
-
-    SDL_FreeSurface(imagem_bola);
+	SDL_FreeSurface(imagem_bola);
 }
 
-// renderiza as bolas em suas respectivas posições
-void renderizaCena(Bola* bola, int n_bolas){
+void renderizaCena(Bola* bola, int n_bolas) {
 
-    for(int i = 0; i < n_bolas; i++){
+	for (int i = 0; i < n_bolas; i++) {
 
-        bola_atual.h = bola[i].r * 2;
-        bola_atual.w = bola[i].r * 2;
-        bola_atual.x = bola[i].x;
-        bola_atual.y = bola[i].y;
+		bola_atual.h = bola[i].r * 2;
+		bola_atual.w = bola[i].r * 2;
+		bola_atual.x = bola[i].x;
+		bola_atual.y = bola[i].y;
 
-        SDL_RenderCopy(render, textura_bola, NULL, &bola_atual);
-    }
+		SDL_RenderCopy(render, textura_bola, NULL, &bola_atual);
+	}
 }
 
-// retorna o centro da bola
-int achaCentro(int c){
+int sobreposicao(Bola* bola, int a, int b) {
 
-    return (raio + c);
+	double dist_centros, x, y;
+
+	x = (bola[a].x + raio) - (bola[b].x + raio);
+	y = (bola[a].y + raio) - (bola[b].y + raio);
+
+	dist_centros = x * x + y * y;
+
+	if (dist_centros <= (pow(raio, 2.0) * 4) + 5)
+		return 1;
+
+	return 0;
 }
 
-// checa se as bolas criadas estão sobrepostas
-int sobreposicao(Bola* bola, int i, int j){
+void criaBolas(Bola* bola, int n_bolas, int v_max, int v_min, int WINDOW_WIDTH, int WINDOW_HEIGHT) {
 
-    double distancia_centros, x, y;
+	int i, j = 0;
 
-    x = achaCentro(bola[i].x) - achaCentro(bola[j].x);
-    y = achaCentro(bola[i].y) - achaCentro(bola[j].y);
-    distancia_centros = x*x + y*y;
+	for (i = 0; i < n_bolas; i++) {
 
-    if(distancia_centros <= (pow(raio,2) * 4) + 5)
-        return 1;
+		bola[i].r = raio;
 
-    return 0;
+		bola[i].vx = v_min + (rand() % (v_max - v_min));
+		bola[i].vy = v_min + (rand() % (v_max - v_min));
+
+		bola[i].x = (rand() % (WINDOW_WIDTH - (raio * 2))) + 1;
+		bola[i].y = (rand() % (WINDOW_HEIGHT - (raio * 2))) + 1;
+	}
+
+	for (i = 0; i < n_bolas; i++) {
+
+		for (j = i + 1; j < n_bolas; j++) {
+
+			if (sobreposicao(bola, i, j)) {
+
+				bola[i].x = (rand() % (WINDOW_WIDTH - (raio * 2))) + 1;
+				bola[i].y = (rand() % (WINDOW_HEIGHT - (raio * 2))) + 1;
+
+				i = 0;
+				j = 1;
+			}
+		}
+	}
 }
 
-//cria as bolas com parametros aleatórios
-void criaBolas(Bola* bola, int n_bolas, int v_max, int v_min, int WINDOW_WIDTH, int WINDOW_HEIGHT){
+void atualizaPosicao(Bola* bola, int n_bolas) {
 
-    int i, j = 0;
+	for (int i = 0; i < n_bolas; i++) {
 
-    for(i = 0; i < n_bolas; i++){
+		bola[i].x += bola[i].vx;
+		bola[i].y += bola[i].vy;
 
-        bola[i].r = raio;
-
-        bola[i].vx = v_min + (rand() % (v_max - v_min));
-        bola[i].vy = v_min + (rand() % (v_max - v_min));
-
-        bola[i].x = (rand() % (WINDOW_WIDTH - (raio * 2))) + 1;
-        bola[i].y = (rand() % (WINDOW_HEIGHT - (raio * 2))) + 1;
-    }
-
-    for(i = 0; i  < n_bolas; i++){
-
-        for(j = i + 1; j < n_bolas; j++){
-
-            if( sobreposicao(bola, i, j) == 1 ){
-
-                bola[i].x = (rand() % (WINDOW_WIDTH - (raio * 2))) + 1;
-                bola[i].y = (rand() % (WINDOW_HEIGHT - (raio * 2))) + 1;
-
-                i = 0;
-                j = 1;
-            }
-        }
-    }
+	}
 }
 
 
-//atualiza velocidade apos colisao entre as bolas
-void atualizaVelocidade (Bola* bola, int i, int j) {
+void atualizaVelocidade(Bola* bola, int a, int b) {
 
-    //eixo de colisao
-    double colisao_x = achaCentro(bola[i].x) - achaCentro(bola[j].x);
-    double colisao_y = achaCentro(bola[i].y) - achaCentro(bola[j].y);
+	//eixo de colisao
+	double colisao_x = (bola[a].x + raio) - (bola[b].x + raio);
+	double colisao_y = (bola[a].y + raio) - (bola[b].y + raio);
 
-    double colisao = colisao_x*colisao_x + colisao_y*colisao_y;
+	double colisao = colisao_x * colisao_x + colisao_y * colisao_y;
 
-    //velocidades projetadas
-    double pvx1 = ((bola[i].vx * colisao_x) + (bola[i].vy * colisao_y)) * colisao_x / colisao;
-    double pvy1 = ((bola[i].vx * colisao_x) + (bola[i].vy * colisao_y)) * colisao_y / colisao;
-    double pvx2 = ((bola[j].vx * colisao_x) + (bola[j].vy * colisao_y)) * colisao_x / colisao;
-    double pvy2 = ((bola[j].vx * colisao_x) + (bola[j].vy * colisao_y)) * colisao_y / colisao;
+	//velocidades projetadas
+	double vx1_proj = ((bola[a].vx * colisao_x) + (bola[a].vy * colisao_y)) * colisao_x / colisao;
+	double vy1_proj = ((bola[a].vx * colisao_x) + (bola[a].vy * colisao_y)) * colisao_y / colisao;
+	double vx2_proj = ((bola[b].vx * colisao_x) + (bola[b].vy * colisao_y)) * colisao_x / colisao;
+	double vy2_proj = ((bola[b].vx * colisao_x) + (bola[b].vy * colisao_y)) * colisao_y / colisao;
 
-    //inverte as velocidades que estao no EIXO de colisao
-    bola[i].vx -= (pvx1 - pvx2);
-    bola[i].vy -= (pvy1 - pvy2);
-    bola[j].vx -= (pvx2 - pvx1);
-    bola[j].vy -= (pvy2 - pvy1);
+	//inverte as velocidades que estao no EIXO de colisao
+	bola[a].vx -= (vx1_proj - vx2_proj);
+	bola[a].vy -= (vy1_proj - vy2_proj);
+	bola[b].vx -= (vx2_proj - vx1_proj);
+	bola[b].vy -= (vy2_proj - vy1_proj);
 
-    //separa bolas grudadas
-    if(colisao_x != 0 && colisao_y != 0){
+	//separa bolas grudadas
+	if (colisao_x != 0 && colisao_y != 0) {
 
-        //soma ou diminui um pixel na posiçao das bolas
-        bola[i].x += colisao_x / abs(colisao_x);
-        bola[i].y += colisao_y / abs(colisao_y);
+		//soma ou diminui um pixel na posiçao das bolas
+		bola[a].x += colisao_x / fabs(colisao_x);
+		bola[a].y += colisao_y / fabs(colisao_y);
 
-        bola[j].x -= colisao_x / abs(colisao_x);
-        bola[j].y -= colisao_y / abs(colisao_y);
-    }
+		bola[b].x -= colisao_x / fabs(colisao_x);
+		bola[b].y -= colisao_y / fabs(colisao_y);
+	}
+
 }
 
-// atualiza posição para cada iteração
-void atualizaPosicao(Bola* bola, int n_bolas){
+void hitDetection(Bola* bola, int n_bolas, int WINDOW_WIDTH, int WINDOW_HEIGHT) {
 
-    for(int i = 0; i < n_bolas; i++){
+	double dist_centros, x, y;
 
-        bola[i].x += bola[i].vx;
-        bola[i].y += bola[i].vy;
-    }
-}
+	for (int i = 0; i < n_bolas; i++) {
 
-//confere se há colisões
-void hitDetection(Bola* bola, int n_bolas, int WINDOW_WIDTH, int WINDOW_HEIGHT){
+		if ((bola[i].x <= 0 && bola[i].vx <= 0) || (bola[i].x + 2 * raio >= WINDOW_WIDTH && bola[i].vx >= 0))
+			bola[i].vx *= -1;
 
-	    double distancia_centros;
-    double x, y;
+		if ((bola[i].y <= 0 && bola[i].vy <= 0) || (bola[i].y + 2 * raio >= WINDOW_HEIGHT && bola[i].vy >= 0))
+			bola[i].vy *= -1;
 
-    for(int i = 0; i < n_bolas; i++){
+	}
 
-        for(int j = i + 1; j < n_bolas; j++){
 
-            x = achaCentro(bola[i].x) - achaCentro(bola[j].x);
-            y = achaCentro(bola[i].y) - achaCentro(bola[j].y);
-            distancia_centros = x*x + y*y;
+	for (int i = 0; i < n_bolas; i++) {
 
-            if(distancia_centros <= pow(raio,2) * 4)
-                atualizaVelocidade(bola, i, j);
+		for (int j = i + 1; j < n_bolas; j++) {
 
-        }
-    }
+			x = (bola[i].x + raio) - (bola[j].x + raio);
+			y = (bola[i].y + raio) - (bola[j].y + raio);
+			dist_centros = pow(x, 2.0) + pow(y, 2.0);
 
-    for(int i = 0; i < n_bolas; i++){
+			if (dist_centros <= pow(raio, 2.0) * 4)
+				atualizaVelocidade(bola, i, j);
 
-        if((bola[i].x <= 0 && bola[i].vx <= 0 )|| (bola[i].x  + 2 * raio >= WINDOW_WIDTH && bola[i].vx >= 0))
-            bola[i].vx *= -1;
+		}
+	}
 
-        if((bola[i].y <= 0 && bola[i].vy <= 0 )||( bola[i].y + 2 * raio >= WINDOW_HEIGHT && bola[i].vy >= 0))
-            bola[i].vy *= -1;
-    }
 }
 
 
-int main(int argc, char** argv){
 
-    int WINDOW_WIDTH;
-    int WINDOW_HEIGHT;
-    int n_bolas;
-    int v_max;
-    int v_min;
+int main(int argc, char** argv) {
 
-    int executando = 1;
+	int WINDOW_WIDTH, WINDOW_HEIGHT;
+	int n_bolas, v_max, v_min;
+	double energia_cinetica = 0;
 
-    SDL_Event fim;
+	int executando = 1;
 
-    srand(time(NULL));
+	SDL_Event fim;
 
-    switch(argc){
+	srand(time(NULL));
 
-        case 1 :
-            WINDOW_WIDTH = 800;
-            WINDOW_HEIGHT = 600;
-            n_bolas = 4;
-            v_max = 5;
-            printf("Primeiro argumento é a LARGURA. Segundo argumento é a ALTURA. Terceiro argumento é o NUMERO de BOLAS. Quarto argumento é a velocidade MAXIMA das BOLAS.\n");
-            break;
+	switch (argc) {
 
-        case 2 :
-            WINDOW_WIDTH = atoi(argv[1]);
-            WINDOW_HEIGHT = 600;
-            n_bolas = 4;
-            v_max = 5;
-            printf("Primeiro argumento é a LARGURA. Segundo argumento é a ALTURA. Terceiro argumento é o NUMERO de BOLAS. Quarto argumento é a velocidade MAXIMA das BOLAS.\n");
-            break;
+	case 1:
+		WINDOW_WIDTH = 800;
+		WINDOW_HEIGHT = 600;
+		n_bolas = 4;
+		v_max = 10;
+		printf("1o parametro e a LARGURA. 2o parametro e a ALTURA. 3o parametro e o NUMERO de BOLAS. 4o parametro e a velocidade MAXIMA das BOLAS.\n");
+		break;
 
-        case 3 :
-            WINDOW_WIDTH = atoi(argv[1]);
-            WINDOW_HEIGHT = atoi(argv[2]);
-            n_bolas = 4;
-            v_max = 5;
-            printf("Primeiro argumento é a LARGURA. Segundo argumento é a ALTURA. Terceiro argumento é o NUMERO de BOLAS. Quarto argumento é a velocidade MAXIMA das BOLAS.\n");
-            break;
+	case 2:
+		WINDOW_WIDTH = atoi(argv[1]);
+		WINDOW_HEIGHT = 600;
+		n_bolas = 4;
+		v_max = 10;
+		printf("1o parametro e a LARGURA. 2o parametro e a ALTURA. 3o parametro e o NUMERO de BOLAS. 4o parametro e a velocidade MAXIMA das BOLAS.\n");
+		break;
 
-        case 4 :
-            WINDOW_WIDTH = atoi(argv[1]);
-            WINDOW_HEIGHT = atoi(argv[2]);
-            n_bolas = atoi(argv[3]);
-            v_max = 5;
-            printf("Primeiro argumento é a LARGURA. Segundo argumento é a ALTURA. Terceiro argumento é o NUMERO de BOLAS. Quarto argumento é a velocidade MAXIMA das BOLAS.\n");
-            break;
+	case 3:
+		WINDOW_WIDTH = atoi(argv[1]);
+		WINDOW_HEIGHT = atoi(argv[2]);
+		n_bolas = 4;
+		v_max = 10;
+		printf("1o parametro e a LARGURA. 2o parametro e a ALTURA. 3o parametro e o NUMERO de BOLAS. 4o parametro e a velocidade MAXIMA das BOLAS.\n");
+		break;
 
-        default :
-            WINDOW_WIDTH = atoi(argv[1]);
-            WINDOW_HEIGHT = atoi(argv[2]);
-            n_bolas = atoi(argv[3]);
-            v_max = atoi(argv[4]);
-            break;
-    }
+	case 4:
+		WINDOW_WIDTH = atoi(argv[1]);
+		WINDOW_HEIGHT = atoi(argv[2]);
+		n_bolas = atoi(argv[3]);
+		v_max = 10;
+		printf("1o parametro e a LARGURA. 2o parametro e a ALTURA. 3o parametro e o NUMERO de BOLAS. 4o parametro e a velocidade MAXIMA das BOLAS.\n");
+		break;
 
-    if( WINDOW_HEIGHT < 100 || WINDOW_HEIGHT > 3000 || WINDOW_WIDTH < 100 || WINDOW_WIDTH > 3000 || n_bolas < 1 || n_bolas > 10 || v_max < 1 || v_max > 10 ){
-            printf("\nDados Invalidos. Utilize valores de LARGURA e ALTURA entre 100 e 3000. Numero de bolas e velocidade maxima entre 1 e 10.\n");
-            return 1;
-    }
+	default:
+		WINDOW_WIDTH = atoi(argv[1]);
+		WINDOW_HEIGHT = atoi(argv[2]);
+		n_bolas = atoi(argv[3]);
+		v_max = atoi(argv[4]);
+		break;
+	}
 
-    v_min = v_max * (-1);
+	if (WINDOW_HEIGHT < 100 || WINDOW_HEIGHT > 3000 || WINDOW_WIDTH < 100 || WINDOW_WIDTH > 3000
+		|| n_bolas < 1 || n_bolas > 10 || v_max < 1 || v_max > 10) {
 
-    Bola* bola = (Bola*) malloc (sizeof (Bola) * n_bolas);
+		printf("\n Parametros Invalidos.\nUtilize valores entre 100 e 3000 para altura e largura da tela e entre 1 e 10 para n de bolas e vel. maxima");
+		return 1;
+	}
 
-    criaJanela(WINDOW_WIDTH, WINDOW_HEIGHT);
+	v_min = v_max * (-1);
 
-    aplicaTextura();
+	Bola* bola = (Bola*)malloc(sizeof(Bola) * n_bolas);
 
-    criaBolas(bola, n_bolas, v_max, v_min, WINDOW_WIDTH, WINDOW_HEIGHT);
+	criaJanela(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    printf("\nPressione a tecla \"x\" para fechar o programa.\n\n");
+	aplicaTextura();
 
+	criaBolas(bola, n_bolas, v_max, v_min, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    while(executando == 1){
+	printf("\n PRESSIONE |X| PARA ENCERRAR O PROGRAMA\n\n");
 
-        while(SDL_PollEvent(&fim) != 0){
-            // fecha quando pressiona tecla X
-            if(fim.key.keysym.sym == SDLK_x)
-                executando = 0;
-        }
+	while (executando == 1) {
 
+		while (SDL_PollEvent(&fim) != 0) {
+			if (fim.key.keysym.sym == SDLK_x)
+				executando = 0;
+		}
 
-        // limpa a tela
-        SDL_RenderClear (render);
+		SDL_RenderClear(render);
 
-        // define a tela como branco
-        SDL_SetRenderDrawColor ( render, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
 
-        // renderiza as bolas na tela
-        renderizaCena(bola, n_bolas);
+		renderizaCena(bola, n_bolas);
 
-        // atualiza a posição das bolas somando a velocidade
-        atualizaPosicao(bola, n_bolas);
+		atualizaPosicao(bola, n_bolas);
 
-        // checa se tem alguma colisão
-        hitDetection(bola, n_bolas, WINDOW_WIDTH, WINDOW_HEIGHT);
+		hitDetection(bola, n_bolas, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-        // mostra um novo render
-        SDL_RenderPresent (render);
+		SDL_RenderPresent(render);
 
-        double energia_cinetica = 0;
+		energia_cinetica = 0;
 
-        for(int i = 0; i < n_bolas; i++)
-            energia_cinetica += (bola[i].vx*bola[i].vx) + (bola[i].vy*bola[i].vy);
+		for (int i = 0; i < n_bolas; i++)
+			energia_cinetica += pow(bola[i].vx, 2.0) + pow(bola[i].vy, 2.0);
 
-        printf("Energia Cinética Total: %.2f\r", energia_cinetica/2);
-    }
+		printf("Energia Cinetica Total = %.2f\r", energia_cinetica / 2);
+	}
 
-    //desaloca variaveis
-    free(bola);
-    SDL_DestroyTexture(textura_bola);
-    SDL_Destroywindow(render);
-    SDL_DestroyWindow(window);
+	free(bola);
+	SDL_DestroyTexture(textura_bola);
+	SDL_DestroyRenderer(render);
+	SDL_DestroyWindow(window);
 
-    return (0);
+	return 0;
 }
